974a1b82ed7e8d0d044294ae5045b35dd0f67492
Benjamin Thurm <bentrm@posteo.de>
2020-11-11 19:24:56 +0100

    fix: Allow to override concept chip props

--- src/components/ConceptChip.tsx
+++ src/components/ConceptChip.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from "react";
-import {Chip} from "@material-ui/core";
+import {Chip, ChipProps} from "@material-ui/core";
 import {Entity} from "../domain";
 import {useHistory} from "react-router-dom";
 
@@ -8,19 +8,21 @@ type ConceptChipProps = {
     id: string,
     label: string,
     title?: string
-};
+} & Partial<ChipProps>;
 
-const ConceptChip: FC<ConceptChipProps> = ({ conceptType, id, label, title }) => {
+const ConceptChip: FC<ConceptChipProps> = (props) => {
+    const { conceptType, id, label, title, ...otherProps } = props;
     const history = useHistory();
 
     return (
         <Chip
             avatar={<conceptType.Icon/>}
-            variant="outlined"
             label={label}
             title={title}
+            clickable
             onClick={() => history.push(`/${conceptType.path}/${id}`)}
             size="small"
+            {...otherProps}
         />
     )
 }
