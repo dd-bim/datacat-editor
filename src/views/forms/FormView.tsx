8de48363244a8dabad8e44cf64e19d775a227c22
Benjamin Thurm <bentrm@posteo.de>
2020-11-11 19:22:15 +0100

    feat: Form view encapsulating form styles

--- src/views/forms/FormView.tsx
+++ src/views/forms/FormView.tsx
@@ -1,4 +1,30 @@
+import React, {ReactNode} from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        "& > :not(:last-child)": {
+            marginBottom: theme.spacing(1)
+        }
+    }
+}));
+
 export type FormProps<T> = {
     id: string,
     onDelete(value: T): void
+}
+
+export type WithChildren<T> = T & {
+    children?: ReactNode
+}
+
+export default function FormView(props: WithChildren<{}>) {
+    const {children} = props;
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            {children}
+        </div>
+    );
 }
