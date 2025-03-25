e4a77ad96c9c2805cce5a56dd159f47634aeb3b6
Christoph Frey <christoph.frey@htw-dresden.de>
2025-03-25 10:56:41 +0100

    Finished Translation and Updating datacat avaiable in English, Italian, Spanish, Dutch and Chinese Minor Visual fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.71oqibno3ao"
+    "revision": "0.nl3ree2nq3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
