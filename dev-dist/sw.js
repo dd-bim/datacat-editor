b8b57b038580110141f2164a52f5244c60c41ef2
Christoph Frey <christoph.frey@htw-dresden.de>
2025-03-27 15:04:33 +0100

    minor fixes updated gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.nl3ree2nq3g"
+    "revision": "0.btdlftnaaeg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
